ded16077eb79008401199bf962dd3d7cd472d74b
AkshayaSri <tridizi.dev@gmail.com>
2025-10-19 11:54:53 +0530

    fix case-sensitive imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/Home/Homepage';
+import Homepage from './pages/Home/Homepage';
 import CartPage from './components/Cart/CartPage';
 import AboutUs from './pages/About/AboutUs';
 import ContactUs from './pages/Contact/ContactUs';
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<ContactUs />} />
