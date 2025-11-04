b1f548e63690c4da331738f88000347de035e23c
AkshayaSri <tridizi.dev@gmail.com>
2025-11-04 18:47:40 +0530

    responsiveness

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -64,7 +64,7 @@ const handleSearchSubmit = (e) => {
       'scrunchies': 'scrunchies',
       'scrunchie': 'scrunchies',
       'claws': 'claw-clips',
-      'Claws': 'claw-clips',
+      'claw clips': 'claw-clips',
       'claw': 'claw-clips',
       'hair bows': 'hair-bows',
       'hair bow': 'hair-bows',
